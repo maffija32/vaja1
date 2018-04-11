21b371f74afb58ca8326da92fb1c22411eaab342
študent <student@c.scng.si>
2018-04-11 11:23:23 +0200

    Začetni komit

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Tabela a = new Tabela(10);
+            a.Dodaj(6);
+            a.Dodaj(24);
+            a.Dodaj(80);
+            a.Dodaj(4);
+            a.Dodaj(19);
+            a.Dodaj(84);
+            a.Dodaj(1);
+            a.Dodaj(10);
+            a.Dodaj(13);
+            a.Dodaj(7);
+
+            Console.WriteLine("Neurejena tabela");
+            Console.Write("\t");
+            a.Izpis();
+
+            a.QuickSort(0, 9);
+            Console.WriteLine("Urejena tabela");
+            Console.Write("\t");
+            a.Izpis();
+            */
+
+            Tabela a = new Tabela(30000);
+            Random r = new Random();
+
+            for(int k = 0; k< 30000; k++)
+            {
+                a.Dodaj(r.Next(30000));
+            }
+
+            DateTime d = DateTime.Now;
+            a.QuickSort(0, 29999);
+            DateTime d1 = DateTime.Now;
+            TimeSpan ts = d1 - d;
+            Console.WriteLine("QuickSort " + ts.Milliseconds + " ms");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QuickSort/QuickSort/tabela.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Tabela
+    {
+        int[] tab;
+        int stElementov;
+
+        public Tabela(int velikost)
+        {
+            tab = new int[velikost];
+            stElementov = 0;
+        }
+
+        public void Dodaj(int k)
+        {
+            tab[stElementov++] = k;
+        }
+
+        public void Izpis()
+        {
+            for(int k=0; k<tab.Length; k++)
+            {
+                Console.Write(tab[k] + "\t");
+            }
+
+            /*
+            foreach(int x in tab)
+            {
+                Console.Write(x + "\t");
+            }
+            */
+
+            Console.WriteLine();
+        }
+
+        public int Pivot(int zac, int konec)
+        {
+            int p = tab[zac];
+            int m = zac;
+            int n = konec + 1;
+
+            //levo išči prvega večjega od p
+            do
+            {
+                m++;
+            } while (tab[m] <= p & m < konec);
+
+            //desno išči prvega manjšega od p
+            do
+            {
+                n--;
+            } while (tab[n] > p);
+
+            while (m < n)
+            {
+                //zamenjaj tab[m] in tab[n]
+                int temp = tab[m];
+                tab[m] = tab[n];
+                tab[n] = temp;
+
+                //nadaljuj iskanje
+                do
+                {
+                    m++;
+                } while (tab[m] <= p);
+
+                do
+                {
+                    n--;
+                } while (tab[n] > p);
+            }
+
+            //zamenjaj pivot in element na n-tem mestu
+            int temp1 = tab[zac];
+            tab[zac] = tab[n];
+            tab[n] = temp1;
+
+            return n;
+        }
+
+        public void QuickSort(int zac, int konec)
+        {
+            if (zac >= konec)
+                return;
+
+            int k = Pivot(zac, konec);
+            QuickSort(zac, k - 1);
+            QuickSort(k + 1, konec);
+        }
+    }
+}
